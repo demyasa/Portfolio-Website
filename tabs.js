8cc6ed3f073ee58bbc133ffd8309d1eeea6ab617
demyasa <dmyasnyankin@sandiego.edu>
2025-06-02 19:09:10 -0700

    Update JS to load content for work experience section

--- tabs.js
+++ tabs.js
@@ -4,21 +4,24 @@ const jobs = document.querySelectorAll('.job');
 const jobsContainer = document.querySelector('.jobs');
 const jobContent = document.querySelectorAll('.job-box');
 
-// jobs.forEach(t => t.addEventListener('click', () => {
-//     console.log("YO MOMMA");
-// }))
-
 // Event Delegation
 
 jobsContainer.addEventListener('click', (e) => {
     // Get closest parent with relevant class
     const clicked = e.target.closest('.job');
-    console.log(clicked);
+    // console.log(clicked);
 
     // Guard Clause: Ignore clicks where closest result is null
     // aka return out of function
     if(!clicked) return;
 
+    // Remove active classes
     jobs.forEach(job => job.classList.remove('job-active'));
+    jobContent.forEach(content => content.classList.remove('job-box--active'));
+
+    // Activate job tab
     clicked.classList.add('job-active');
+
+    // Load content
+    document.querySelector(`.job-box-${clicked.dataset.job}`).classList.add('job-box--active');
 })
